cd905706e12d35091a37ba4c757eb12f74bf9eb4
wb_yaolei02 <wb_yaolei02@meituan.com>
2023-07-06 15:23:00 +0800

    add

--- src/main/java/com/play4fun/utils/CleanQueryCacheJob.java
+++ src/main/java/com/play4fun/utils/CleanQueryCacheJob.java
@@ -1,58 +1,69 @@
 package com.play4fun.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+@Slf4j
 public class CleanQueryCacheJob {
-    private static final String CACHE_FILE_PATH_PREFIX = "/users/leoyao/efs/cdp/data";
+    private static final String CACHE_FILE_PATH = "/users/leoyao/efs/cdp/data";
     private static final String DATE_PATTERN = "yyyyMMdd";
 
     public static void main(String[] args) throws ParseException {
         System.out.println(System.getProperty("user.dir"));
-        cleanExpiredQueryCacheTask("111222333");
+        cleanExpiredQueryCacheTask();
     }
 
+
 //    @Crane("com.sankuai.sjstcdp.rms.ups.clean-expired-query-cache-task")
-    public static void cleanExpiredQueryCacheTask(String tenantId) throws ParseException {
-        //queryClient cache过期时间
-        int queryCacheExpiredTime = 30;
+    public static void cleanExpiredQueryCacheTask() {
+        try {
+            //queryClient cache过期时间
+            int queryCacheExpiredTime = 7;
 
-        //当前租户文件路径
-        String filePath = CACHE_FILE_PATH_PREFIX + File.separator + tenantId;
+            //删除文件截止时间
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_MONTH, -queryCacheExpiredTime);
+            Date deleteEndTime = calendar.getTime();
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
 
-        //删除文件截止时间
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, -queryCacheExpiredTime); // 将日期减去指定天
-        Date date = calendar.getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
-        String deleteTime = sdf.format(date);
+            //追溯时间
+            String queryCacheExpireStartTime = "20220101";
+            Date deleteStartTime = sdf.parse(queryCacheExpireStartTime);
 
-        //追溯时间
-        String queryCacheExpireStartTime = "20220101";
-
-        Date deleteEndTime = sdf.parse(deleteTime);
-        Date deleteStartTime = sdf.parse(queryCacheExpireStartTime);
-
-        //找出所有满足条件的文件夹
-        File userFolder = new File(filePath);
-        if (userFolder.isDirectory()) {
-            File[] files = userFolder.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isDirectory()) {
-                        String folderName = file.getName();
-                        Date folderTime = sdf.parse(folderName);
-                        if (folderTime.compareTo(deleteStartTime) >= 0 && folderTime.compareTo(deleteEndTime) <= 0) {
-                            deleteFolder(file);
-                        }
-                    }
-                }
+            File dataFolder = new File(CACHE_FILE_PATH);
+            File[] dataFiles = dataFolder.listFiles();
+            if (dataFiles == null) {
+                log.info(CACHE_FILE_PATH + "路径下无子文件");
+                return;
             }
+            //删除过期文件夹
+            for (File dataFile : dataFiles) {
+//                if (dataFile.isDirectory()) {
+//                    File[] files = dataFile.listFiles();
+//                    if (files != null) {
+                        for (File file : dataFiles) {
+                            if (file.isDirectory()) {
+                                String folderName = file.getName();
+                                Date folderTime = sdf.parse(folderName);
+                                if (folderTime.compareTo(deleteStartTime) >= 0 && folderTime.compareTo(deleteEndTime) <= 0) {
+                                    deleteFolder(file);
+                                }
+                            }
+                        }
+//                    }
+//                }
+            }
+        } catch (ParseException e) {
+            log.error("sdf.parse failed", e);
+        } catch (Exception e) {
+            log.error("CacheCleanJob failed", e);
+            throw e;
         }
-
     }
 
     private static void deleteFolder(File folder) {
@@ -64,6 +75,7 @@ public class CleanQueryCacheJob {
                 }
             }
         }
+        log.info("删除文件: " + folder.getAbsolutePath());
         folder.delete();
     }
 }
